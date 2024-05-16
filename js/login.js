1327e2296d6ee0edd82b0a14b07a7e61ac86bafb
minkyung <mxkxx@naver.com>
2024-05-16 17:45:32 +0900

    ✨ Add validation to input

--- js/login.js
+++ js/login.js
@@ -2,13 +2,70 @@ const userEmail = document.querySelector('#email');
 const userPwd = document.querySelector('#password');
 const loginBtn = document.querySelector('.btn-login');
 
-function loginCheckInput() {
-  const emailCheck = userEmail.value;
-  const passwordCheck = userPwd.value;
+const errorMsgEmail = document.querySelector('.error-msg-email');
+const errorMsgPwd = document.querySelector('.error-msg-pwd');
 
-  const isDisabled = emailCheck === '' || passwordCheck === '';
-  loginBtn.disabled = isDisabled;
+// 이메일 유효성
+function emailCheck(email) {
+  const email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+  return email_regex.test(email);
+}
+
+function validEmail() {
+  const email = userEmail.value;
+
+  if (email === '') {
+    errorMsgEmail.textContent = '이메일을 입력해주세요.';
+    userEmail.classList.add('error-input');
+  } else {
+    if (emailCheck(email)) {
+      errorMsgEmail.textContent = '';
+      userEmail.classList.remove('error-input');
+    } else {
+      errorMsgEmail.textContent = '잘못된 이메일 형식입니다.';
+      userEmail.classList.add('error-input');
+    }
+  }
+}
+
+// 비밀번호 길이 체크
+function passwordCheck(pwd) {
+  return pwd.length >= 8;
+}
+
+function validPwd() {
+  const pwd = userPwd.value;
+
+  if (pwd === '') {
+    errorMsgPwd.textContent = '비밀번호를 입력해주세요.';
+    userPwd.classList.add('error-input');
+  } else {
+    if (passwordCheck(pwd)) {
+      errorMsgPwd.textContent = '';
+      userPwd.classList.remove('error-input');
+    } else {
+      errorMsgPwd.textContent = '비밀번호를 8자 이상 입력해주세요.';
+      userPwd.classList.add('error-input');
+    }
+  }
 }
 
 const userInputs = [userEmail, userPwd];
+const valids = [validEmail, validPwd];
+
+for (let i = 0; i < valids.length; i++) userInputs[i].addEventListener('focusout', valids[i]);
+for (let i = 0; i < valids.length; i++) userInputs[i].addEventListener('keyup', valids[i]);
+
+// 로그인 버튼 비활성화
+function loginCheckInput() {
+  const emailValue = userEmail.value;
+  const passwordValue = userPwd.value;
+
+  const isEmpty = emailValue === '' || passwordValue === ''; // 비어있거나
+  const isError = errorMsgEmail.textContent !== '' || errorMsgPwd.textContent !== ''; // 에러가 있거나(에러메시지로 체크)
+
+  loginBtn.disabled = isError || isEmpty;
+}
+
 userInputs.forEach(input => input.addEventListener('input', loginCheckInput));
+userInputs.forEach(input => input.addEventListener('keyup', loginCheckInput));
--- js/signup.js
+++ js/signup.js
@@ -5,16 +5,108 @@ const userPwdRepeat = document.querySelector('#password-repeat');
 
 const loginBtn = document.querySelector('.btn-login');
 
-function joinCheckInput() {
-  const emailCheck = userEmail.value;
-  const nicknameCheck = userNickname.value;
-  const passwordCheck = userPwd.value;
-  const pwdRepeatCheck = userPwdRepeat.value;
+const errorMsgEmail = document.querySelector('.error-msg-email');
+const errorMsgNickname = document.querySelector('.error-msg-nickname');
+const errorMsgPwd = document.querySelector('.error-msg-pwd');
+const errorMsgRepeat = document.querySelector('.error-msg-repeat');
 
-  const isDisabled = emailCheck === '' || nicknameCheck === '' || passwordCheck === '' || pwdRepeatCheck === '';
-
-  loginBtn.disabled = isDisabled;
+// 이메일 유효성
+function emailCheck(email) {
+  const email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+  return email_regex.test(email);
 }
 
-const userInputs = [userEmail, userPwd, userNickname, userPwdRepeat];
+function validEmail() {
+  const email = userEmail.value;
+
+  if (email === '') {
+    errorMsgEmail.textContent = '이메일을 입력해주세요.';
+    userEmail.classList.add('error-input');
+  } else {
+    if (emailCheck(email)) {
+      errorMsgEmail.textContent = '';
+      userEmail.classList.remove('error-input');
+    } else {
+      errorMsgEmail.textContent = '잘못된 이메일 형식입니다.';
+      userEmail.classList.add('error-input');
+    }
+  }
+}
+
+function validNickname() {
+  const nickname = userNickname.value;
+
+  if (nickname === '') {
+    errorMsgNickname.textContent = '닉네임을 입력해주세요';
+    userNickname.classList.add('error-input');
+  } else {
+    errorMsgNickname.textContent = '';
+    userNickname.classList.remove('error-input');
+  }
+}
+
+// 비밀번호 길이 체크
+function passwordCheck(pwd) {
+  return pwd.length >= 8;
+}
+
+function validPwd() {
+  const pwd = userPwd.value;
+
+  if (pwd === '') {
+    errorMsgPwd.textContent = '비밀번호를 입력해주세요.';
+    userPwd.classList.add('error-input');
+  } else {
+    if (passwordCheck(pwd)) {
+      errorMsgPwd.textContent = '';
+      userPwd.classList.remove('error-input');
+    } else {
+      errorMsgPwd.textContent = '비밀번호를 8자 이상 입력해주세요.';
+      userPwd.classList.add('error-input');
+    }
+  }
+}
+
+function isMatch(pwd, rep) {
+  return pwd === rep;
+}
+
+function validRepeat() {
+  const pwd = userPwd.value;
+  const rep = userPwdRepeat.value;
+
+  if (isMatch(pwd, rep)) {
+    errorMsgRepeat.textContent = '';
+    userPwdRepeat.classList.add('error-input');
+  } else {
+    errorMsgRepeat.textContent = '비밀번호가 일치하지 않습니다.';
+    userPwdRepeat.classList.remove('error-input');
+  }
+}
+
+function joinCheckInput() {
+  const emailValue = userEmail.value;
+  const nicknameValue = userNickname.value;
+  const passwordValue = userPwd.value;
+  const pwdRepeatValue = userPwdRepeat.value;
+
+  const isEmpty = emailValue === '' || nicknameValue === '' || passwordValue === '' || pwdRepeatValue === '';
+  const isError =
+    errorMsgEmail.textContent !== '' ||
+    errorMsgPwd.textContent !== '' ||
+    errorMsgNickname.textContent !== '' ||
+    errorMsgRepeat.textContent !== '';
+
+  loginBtn.disabled = isError || isEmpty;
+}
+
+const userInputs = [userEmail, userNickname, userPwd, userPwdRepeat];
+const valids = [validEmail, validNickname, validPwd, validRepeat];
+
+for (let i = 0; i < valids.length; i++) userInputs[i].addEventListener('focusout', valids[i]);
+for (let i = 0; i < valids.length; i++) userInputs[i].addEventListener('keyup', valids[i]);
+// 스프린트4 : 스프린트미션 피그마에는 'focusout할 때'라고 설명했지만 keyup 이벤트도 추가해야 깔끔하게 동작하는거 같아서 추가
+// 맞는 방법인진 모르겠다.
+
 userInputs.forEach(input => input.addEventListener('input', joinCheckInput));
+userInputs.forEach(input => input.addEventListener('keyup', joinCheckInput));
