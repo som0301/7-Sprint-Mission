aaf3a36dde560c22fe836ef40e326dc0fe9d4dff
minkyung <mxkxx@naver.com>
2024-05-16 13:55:15 +0900

    🎨 Apply prettier
    prettier 적용
    pr review 수정

--- js/login.js
+++ js/login.js
@@ -2,16 +2,13 @@ const userEmail = document.querySelector('#email');
 const userPwd = document.querySelector('#password');
 const loginBtn = document.querySelector('.btn-login');
 
-function loginCheckInput(){
-    const emailCheck = userEmail.value;
-    const passwordCheck = userPwd.value;
+function loginCheckInput() {
+  const emailCheck = userEmail.value;
+  const passwordCheck = userPwd.value;
 
-    if((emailCheck === '') || (passwordCheck === '')){
-        loginBtn.disabled = true;
-    } else {
-        loginBtn.disabled = false;
-    }
+  const isDisabled = emailCheck === '' || passwordCheck === '';
+  loginBtn.disabled = isDisabled;
 }
 
-const userInputs = [ userEmail, userPwd ];
-userInputs.forEach((input) => input.addEventListener('input', loginCheckInput));
+const userInputs = [userEmail, userPwd];
+userInputs.forEach(input => input.addEventListener('input', loginCheckInput));
--- js/signup.js
+++ js/signup.js
@@ -5,19 +5,16 @@ const userPwdRepeat = document.querySelector('#password-repeat');
 
 const loginBtn = document.querySelector('.btn-login');
 
-function joinCheckInput(){
-    const emailCheck = userEmail.value;
-    const nicknameCheck = userNickname.value;
-    const passwordCheck = userPwd.value;
-    const pwdRepeatCheck = userPwdRepeat.value;
+function joinCheckInput() {
+  const emailCheck = userEmail.value;
+  const nicknameCheck = userNickname.value;
+  const passwordCheck = userPwd.value;
+  const pwdRepeatCheck = userPwdRepeat.value;
 
-    if((emailCheck === '') || (nicknameCheck === '') ||
-        (passwordCheck === '') || (pwdRepeatCheck === '')){
-            loginBtn.disabled = true;
-    } else {
-        loginBtn.disabled = false;
-    }
+  const isDisabled = emailCheck === '' || nicknameCheck === '' || passwordCheck === '' || pwdRepeatCheck === '';
+
+  loginBtn.disabled = isDisabled;
 }
 
-const userInputs = [ userEmail, userPwd, userNickname, userPwdRepeat ];
-userInputs.forEach((input) => input.addEventListener('input', joinCheckInput));
+const userInputs = [userEmail, userPwd, userNickname, userPwdRepeat];
+userInputs.forEach(input => input.addEventListener('input', joinCheckInput));
