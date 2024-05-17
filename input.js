7a00c77f18c0590c03821337a7d37dbe6eb81594
장아영 <jang-ayoung@jang-ayeong-ui-MacBookPro.local>
2024-05-17 15:40:19 +0900

    로그인 페이지 이메일 입력창 동작, 비밀번호 입력창 동작 구현

--- input.js
+++ input.js
@@ -1,17 +1,48 @@
-//이메일 오류 메시지//
-function validateEmaill(input) {
+const emailInput = document.querySelector('#email')
+emailInput.addEventListener('blur', function(e) {
     const errorMessage = document.querySelector('#email-error');
-    if(!input.value) {
-        innput.classsList.add('error');
+    if(emailInput.value === "") {
+        emailInput.classList.add('error');
         errorMessage.textContent = '이메일을 입력해주세요.';
-    } else if (!isValidEmail(input.value)) {
-        input.classList.add('error');
-        errorMessage.textContennt = '잘못된 이메일 형식입니다.';
+    } else if (!isValidEmail(emailInput.value) && emailInput.value !== "") {
+        emailInput.classList.add('error');
+        errorMessage.textContent = '';
+        errorMessage.textContent = '잘못된 이메일 형식입니다.';
     } else {
-        input.classList.remove('error');
+        emailInput.classList.remove('error');
         errorMessage.textContent = '';
     }
-}
+});
+
+
+const passwordInput = document.querySelector('#password')
+passwordInput.addEventListener('blur', function(e) {
+    const pwErrorMessage = document.querySelector('#password-error');
+    if(passwordInput.value === "") {
+        passwordInput.classList.add('error');
+        pwErrorMessage.textContent = '비밀번호를 입력해주세요.';
+    } else if (!isValidEmail(passwordInput.value) && passwordInput.value !== "" && passwordInput.value.length < 8) {
+        passwordInput.classList.add('error');
+        pwErrorMessage.textContent = '';
+        pwErrorMessage.textContent = '비밀번호를 8자 이상 입력해주세요.';
+    } else {
+        passwordInput.classList.remove('error');
+        pwErrorMessage.textContent = '';
+    }
+});
+// function validateEmail(input) {
+//     const errorMessage = document.querySelector('#email-error');
+//     if(!input.value) {
+//         input.classsList.add('error');
+//         errorMessage.textContent = '이메일을 입력해주세요.';
+//     } else if (!isValidEmail(input.value)) {
+//         input.classList.add('error');
+//         errorMessage.textContennt = '잘못된 이메일 형식입니다.';
+//     } else {
+//         input.classList.remove('error');
+//         errorMessage.textContent = '';
+//     }
+// }
 
 //이메일 유효성 검사//
 function isValidEmail(email) {
