89bbbe2124d6558d829fb82bd2647ff1fe630c3f
ddol9 <drddol9@gmail.com>
2024-05-17 16:24:39 +0900

    feat: 로그인 페이지 비밀번호 검사 기능 추가

--- signin.js
+++ signin.js
@@ -1,5 +1,5 @@
 document.getElementById('email').addEventListener('focusout', function() {
-  const errorMessage = document.getElementById('error-message');
+  const errorMessage = document.getElementById('error-message-email');
   const email = document.getElementById('email').value;
 
   function validateEmail(email) {
@@ -14,6 +14,24 @@ document.getElementById('email').addEventListener('focusout', function() {
   } else if (!validateEmail(email)) {
     this.classList.add('error');
     errorMessage.textContent = '잘못된 이메일 형식입니다';
+    errorMessage.style.display = 'block';
+  } else {
+    this.classList.remove('error');
+    errorMessage.style.display = 'none';
+  }
+});
+
+document.getElementById('password').addEventListener('focusout', function() {
+  const errorMessage = document.getElementById('error-message-password');
+  const password = document.getElementById('password').value;
+
+  if (password === '') {
+    this.classList.add('error');
+    errorMessage.textContent = '비밀번호를 입력해주세요';
+    errorMessage.style.display = 'block';
+  } else if (password.length < 8) {
+    this.classList.add('error');
+    errorMessage.textContent = '비밀번호를 8자 이상 입력해주세요';
     errorMessage.style.display = 'block';
   } else {
     this.classList.remove('error');
