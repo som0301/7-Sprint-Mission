975cc806fa3635978f3113c97e0f3224754cd2b6
gayeong <gy2039587@naver.com>
2024-05-17 12:12:13 +0900

    로그인 버튼

--- js/validateInput.js
+++ js/validateInput.js
@@ -1,12 +1,15 @@
 const emailInput = document.querySelector(".email-input");
-const nicknameInput=document.querySelector(".nickname-input");
+const nicknameInput = document.querySelector(".nickname-input");
 const passwordInput = document.querySelector(".password-input");
-const passwordConfirmInput=document.querySelector(".password-confirm-input");
+const passwordConfirmInput = document.querySelector(".password-confirm-input");
 const emailErrorMessage = document.querySelector(".email-error-message");
-const nicknameErrorMessage=document.querySelector(".nickname-error-message");
+const nicknameErrorMessage = document.querySelector(".nickname-error-message");
 const passwordErrorMessage = document.querySelector(".password-error-message");
-const passwordConfirmMessage=document.querySelector(".password-confirm-error-message");
-const submitButton=document.querySelector(".login-submit");
+const passwordConfirmMessage = document.querySelector(
+  ".password-confirm-error-message"
+);
+const submitButton = document.querySelector(".login_submit");
+const signupForm = document.getElementById("signupForm");
 
 const isEmailValid = (email) => {
   const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
@@ -15,8 +18,8 @@ const isEmailValid = (email) => {
 };
 
 const isPasswordValid = (password) => {
-  const passwordPattern=/^.{8,}$/;
-  const isPassword=passwordPattern.test(password);
+  const passwordPattern = /^.{8,}$/;
+  const isPassword = passwordPattern.test(password);
   return isPassword;
 };
 
@@ -38,9 +41,9 @@ function validateEmail(email) {
   emailErrorMessage.textContent = "";
 }
 
-function validateNickname(nickname){
-  if(nickname===""){
-    nicknameInput.className+=" nickname-error";
+function validateNickname(nickname) {
+  if (nickname === "") {
+    nicknameInput.className += " nickname-error";
     nicknameErrorMessage.classList += " error-message-on";
     nicknameErrorMessage.textContent = "닉네임을 입력해주세요.";
     return;
@@ -57,8 +60,8 @@ function validatePassword(password) {
     passwordErrorMessage.textContent = "비밀번호를 입력해주세요.";
     return;
   }
-  if(!isPasswordValid(password)){
-    passwordInput.className+=" password-error";
+  if (!isPasswordValid(password)) {
+    passwordInput.className += " password-error";
     passwordErrorMessage.classList += " error-message-on";
     passwordErrorMessage.textContent = "비밀번호를 8자 이상 입력해주세요.";
     return;
@@ -68,9 +71,9 @@ function validatePassword(password) {
   passwordErrorMessage.textContent = "";
 }
 
-function validatePasswordConfirm(password,passwordConfirm){
-  if(password!==passwordConfirm){
-    passwordConfirmInput.className+=" password-confirm-error";
+function validatePasswordConfirm(password, passwordConfirm) {
+  if (password !== passwordConfirm) {
+    passwordConfirmInput.className += " password-confirm-error";
     passwordConfirmMessage.classList += " error-message-on";
     passwordConfirmMessage.textContent = "비밀번호가 일치하지 않습니다";
     return;
@@ -80,24 +83,51 @@ function validatePasswordConfirm(password,passwordConfirm){
   passwordConfirmMessage.textContent = "";
 }
 
-function disableSubmitButton(){
-  if(){
-    submitButton.disabled=true;
+function disableSubmitButton() {
+  const inputsAreEmpty =
+    !emailInput.value ||
+    !nicknameInput.value ||
+    !passwordInput.value ||
+    !passwordConfirmInput.value;
+  const errorMessagesAreActive =
+    emailErrorMessage.textContent ||
+    nicknameErrorMessage.textContent ||
+    passwordErrorMessage.textContent ||
+    passwordConfirmMessage.textContent;
+
+  if (inputsAreEmpty || errorMessagesAreActive) {
+    submitButton.disabled = true;
+    submitButton.style.cursor = "default";
+  } else {
+    submitButton.disabled = false;
+    submitButton.style.cursor = "pointer";
+    submitButton.style.backgroundColor = "#3692FF";
   }
 }
 
 emailInput.addEventListener("focusout", () => {
   validateEmail(emailInput.value);
+  disableSubmitButton();
 });
 
 passwordInput.addEventListener("focusout", () => {
   validatePassword(passwordInput.value);
+  disableSubmitButton();
 });
 
-nicknameInput.addEventListener("focusout",()=>{
+nicknameInput.addEventListener("focusout", () => {
   validateNickname(nicknameInput.value);
+  disableSubmitButton();
 });
 
-passwordConfirmInput.addEventListener("focusout",()=>{
-  validatePasswordConfirm(passwordInput.value,passwordConfirmInput.value);
-})
+passwordConfirmInput.addEventListener("focusout", () => {
+  validatePasswordConfirm(passwordInput.value, passwordConfirmInput.value);
+  disableSubmitButton();
+});
+
+signupForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (!submitButton.disabled) {
+    window.location.href = "index.html";
+  }
+});
