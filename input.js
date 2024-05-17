fba4be4bcca948d3a4f5c1c333a9866b2d92a3bd
장아영 <jang-ayoung@jang-ayeong-ui-MacBookPro.local>
2024-05-17 19:38:47 +0900

    닉네임 확인 수정 및 비밀번호 일치 확인기능 추가

--- input.js
+++ input.js
@@ -1,3 +1,4 @@
+//이메일 입력 확인&이메일 형식 확인//
 const emailInput = document.querySelector('#email')
 emailInput.addEventListener('blur', function(e) {
     const errorMessage = document.querySelector('#email-error');
@@ -14,8 +15,8 @@ emailInput.addEventListener('blur', function(e) {
     }
 });
 
-
-const passwordInput = document.querySelector('#password')
+//비밀번호 입력 학인 & 비밀번호 8자이상 확인//
+const passwordInput = document.querySelector('#password');
 passwordInput.addEventListener('blur', function(e) {
     const pwErrorMessage = document.querySelector('#password-error');
     if(passwordInput.value === "") {
@@ -31,31 +32,42 @@ passwordInput.addEventListener('blur', function(e) {
     }
 });
 
+//비밀번호 일치하는지 확인//
+function passwordMatch(password, confirmpassword){
+    return password === confirmpassword;
+};
+//비밀번호 확인 변수 할당//
+const confirmpasswordInput = document.querySelector('#password-check');
+const confirmpwErrorMessage = document.querySelector('#password-confirmation-error');
+
+confirmpasswordInput.addEventListener('blur', function(e){ 
+    if (!passwordMatch(passwordInput.value, confirmpasswordInput.value)){
+        confirmpasswordInput.classList.add('error');
+        confirmpwErrorMessage.textContent = '비밀번호가 일치하지 않습니다.';
+    } else{
+        confirmpasswordInput.classList.remove('error');
+        confirmpwErrorMessage.textContent = '';
+    };
+});
+
+
+//닉네임 입력 여부 확인//
 const nicknameInput = document.querySelector('#nickname')
 nicknameInput.addEventListener('blur', function(e) {
-    const errorMessage = document.querySelector('#nickname-error');
+    const nicknameErrorMessage = document.querySelector('#nickname-error');
     if(nicknameInput.value === "") {
         nicknameInput.classList.add('error');
-        errorMessage.textContent = '닉네임을 입력해주세요.';
+        nicknameErrorMessage.textContent = '닉네임을 입력해주세요.';
+    } else{
+        nicknameInput.classList.remove('error');
+        nicknameErrorMessage.textContent = '';
     }
 });
 
-// function validateEmail(input) {
-//     const errorMessage = document.querySelector('#email-error');
-//     if(!input.value) {
-//         input.classsList.add('error');
-//         errorMessage.textContent = '이메일을 입력해주세요.';
-//     } else if (!isValidEmail(input.value)) {
-//         input.classList.add('error');
-//         errorMessage.textContennt = '잘못된 이메일 형식입니다.';
-//     } else {
-//         input.classList.remove('error');
-//         errorMessage.textContent = '';
-//     }
-// }
 
-//이메일 유효성 검사//
+//이메일 유효성 검사 함수//
 function isValidEmail(email) {
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailPattern.test(email);
   }
+
