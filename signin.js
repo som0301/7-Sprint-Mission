0d0800f0f540ccf00343d4bcea16a632c8628477
ddol9 <drddol9@gmail.com>
2024-05-19 12:44:45 +0900

    refactor: set Prettier

--- signin.js
+++ signin.js
@@ -8,60 +8,59 @@ function validatePassword(password) {
 }
 
 function btnActive() {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  const button = document.getElementById('button-auth');
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const button = document.getElementById("button-auth");
 
   if (validateEmail(email) && validatePassword(password)) {
-    button.classList.remove('disabled');
+    button.classList.remove("disabled");
     button.disabled = false;
   } else {
-    button.classList.add('disabled');
+    button.classList.add("disabled");
     button.disabled = true;
   }
 }
 
-document.getElementById('email').addEventListener('focusout', function() {
-  const errorMessage = document.getElementById('error-message-email');
-  const email = document.getElementById('email').value;
+document.getElementById("email").addEventListener("focusout", function () {
+  const errorMessage = document.getElementById("error-message-email");
+  const email = document.getElementById("email").value;
 
-  if (email === '') {
-    this.classList.add('error');
-    errorMessage.textContent = '이메일을 입력해주세요';
-    errorMessage.style.display = 'block';
+  if (email === "") {
+    this.classList.add("error");
+    errorMessage.textContent = "이메일을 입력해주세요";
+    errorMessage.style.display = "block";
   } else if (!validateEmail(email)) {
-    this.classList.add('error');
-    errorMessage.textContent = '잘못된 이메일 형식입니다';
-    errorMessage.style.display = 'block';
+    this.classList.add("error");
+    errorMessage.textContent = "잘못된 이메일 형식입니다";
+    errorMessage.style.display = "block";
   } else {
-    this.classList.remove('error');
-    errorMessage.style.display = 'none';
+    this.classList.remove("error");
+    errorMessage.style.display = "none";
   }
   btnActive();
 });
 
-document.getElementById('password').addEventListener('focusout', function() {
-  const errorMessage = document.getElementById('error-message-password');
-  const password = document.getElementById('password').value;
+document.getElementById("password").addEventListener("focusout", function () {
+  const errorMessage = document.getElementById("error-message-password");
+  const password = document.getElementById("password").value;
 
-  if (password === '') {
-    this.classList.add('error');
-    errorMessage.textContent = '비밀번호를 입력해주세요';
-    errorMessage.style.display = 'block';
+  if (password === "") {
+    this.classList.add("error");
+    errorMessage.textContent = "비밀번호를 입력해주세요";
+    errorMessage.style.display = "block";
   } else if (!validatePassword(password)) {
-    this.classList.add('error');
-    errorMessage.textContent = '비밀번호를 8자 이상 입력해주세요';
-    errorMessage.style.display = 'block';
+    this.classList.add("error");
+    errorMessage.textContent = "비밀번호를 8자 이상 입력해주세요";
+    errorMessage.style.display = "block";
   } else {
-    this.classList.remove('error');
-    errorMessage.style.display = 'none';
+    this.classList.remove("error");
+    errorMessage.style.display = "none";
   }
   btnActive();
 });
 
-
-document.getElementById('button-auth').addEventListener('click', function() {
+document.getElementById("button-auth").addEventListener("click", function () {
   if (!this.disabled) {
-    window.location.href = '/items';
+    window.location.href = "/items";
   }
 });
--- signup.js
+++ signup.js
@@ -8,98 +8,106 @@ function validatePassword(password) {
 }
 
 function btnActive() {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  const nickname = document.getElementById('nickname').value;
-  const passwordConfirm = document.getElementById('password-confirm').value;
-  const button = document.getElementById('button-auth');
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const nickname = document.getElementById("nickname").value;
+  const passwordConfirm = document.getElementById("password-confirm").value;
+  const button = document.getElementById("button-auth");
 
   const isEmailVaild = validateEmail(email);
   const isPasswordVaild = validatePassword(password);
-  const isNicknameVaild = nickname !== '';
+  const isNicknameVaild = nickname !== "";
   const isPasswordConfirmVaild = password === passwordConfirm;
 
-  if ( isEmailVaild && isPasswordVaild && isNicknameVaild && isPasswordConfirmVaild) {
-    button.classList.remove('disabled');
+  if (
+    isEmailVaild &&
+    isPasswordVaild &&
+    isNicknameVaild &&
+    isPasswordConfirmVaild
+  ) {
+    button.classList.remove("disabled");
     button.disabled = false;
   } else {
-    button.classList.add('disabled');
+    button.classList.add("disabled");
     button.disabled = true;
   }
 }
 
-document.getElementById('email').addEventListener('focusout', function() {
-  const errorMessage = document.getElementById('error-message-email');
-  const email = document.getElementById('email').value;
+document.getElementById("email").addEventListener("focusout", function () {
+  const errorMessage = document.getElementById("error-message-email");
+  const email = document.getElementById("email").value;
 
-  if (email === '') {
-    this.classList.add('error');
-    errorMessage.textContent = '이메일을 입력해주세요';
-    errorMessage.style.display = 'block';
+  if (email === "") {
+    this.classList.add("error");
+    errorMessage.textContent = "이메일을 입력해주세요";
+    errorMessage.style.display = "block";
   } else if (!validateEmail(email)) {
-    this.classList.add('error');
-    errorMessage.textContent = '잘못된 이메일 형식입니다';
-    errorMessage.style.display = 'block';
+    this.classList.add("error");
+    errorMessage.textContent = "잘못된 이메일 형식입니다";
+    errorMessage.style.display = "block";
   } else {
-    this.classList.remove('error');
-    errorMessage.style.display = 'none';
+    this.classList.remove("error");
+    errorMessage.style.display = "none";
   }
   btnActive();
 });
 
-document.getElementById('nickname').addEventListener('focusout', function() {
-  const errorMessage = document.getElementById('error-message-nickname');
-  const nickname = document.getElementById('nickname').value;
+document.getElementById("nickname").addEventListener("focusout", function () {
+  const errorMessage = document.getElementById("error-message-nickname");
+  const nickname = document.getElementById("nickname").value;
 
-  if (nickname === '') {
-    this.classList.add('error');
-    errorMessage.textContent = '닉네임을 입력해주세요';
-    errorMessage.style.display = 'block';
+  if (nickname === "") {
+    this.classList.add("error");
+    errorMessage.textContent = "닉네임을 입력해주세요";
+    errorMessage.style.display = "block";
   } else {
-    this.classList.remove('error');
-    errorMessage.style.display = 'none';
+    this.classList.remove("error");
+    errorMessage.style.display = "none";
   }
   btnActive();
 });
 
-document.getElementById('password').addEventListener('focusout', function() {
-  const errorMessage = document.getElementById('error-message-password');
-  const password = document.getElementById('password').value;
+document.getElementById("password").addEventListener("focusout", function () {
+  const errorMessage = document.getElementById("error-message-password");
+  const password = document.getElementById("password").value;
 
-  if (password === '') {
-    this.classList.add('error');
-    errorMessage.textContent = '비밀번호를 입력해주세요';
-    errorMessage.style.display = 'block';
+  if (password === "") {
+    this.classList.add("error");
+    errorMessage.textContent = "비밀번호를 입력해주세요";
+    errorMessage.style.display = "block";
   } else if (!validatePassword(password)) {
-    this.classList.add('error');
-    errorMessage.textContent = '비밀번호를 8자 이상 입력해주세요';
-    errorMessage.style.display = 'block';
+    this.classList.add("error");
+    errorMessage.textContent = "비밀번호를 8자 이상 입력해주세요";
+    errorMessage.style.display = "block";
   } else {
-    this.classList.remove('error');
-    errorMessage.style.display = 'none';
+    this.classList.remove("error");
+    errorMessage.style.display = "none";
   }
   btnActive();
 });
 
-document.getElementById('password-confirm').addEventListener('focusout', function() {
-  const errorMessage = document.getElementById('error-message-password-confirm');
-  const password = document.getElementById('password').value;
-  const passwordConfirm = document.getElementById('password-confirm').value;
+document
+  .getElementById("password-confirm")
+  .addEventListener("focusout", function () {
+    const errorMessage = document.getElementById(
+      "error-message-password-confirm",
+    );
+    const password = document.getElementById("password").value;
+    const passwordConfirm = document.getElementById("password-confirm").value;
 
-  if (password !== passwordConfirm) {
-    this.classList.add('error');
-    errorMessage.textContent = '비밀번호가 일치하지 않습니다';
-    errorMessage.style.display = 'block';
-  } else {
-    this.classList.remove('error');
-    errorMessage.style.display = 'none';
-  }
-  btnActive();
-});
+    if (password !== passwordConfirm) {
+      this.classList.add("error");
+      errorMessage.textContent = "비밀번호가 일치하지 않습니다";
+      errorMessage.style.display = "block";
+    } else {
+      this.classList.remove("error");
+      errorMessage.style.display = "none";
+    }
+    btnActive();
+  });
 
-
-document.getElementById('button-auth').addEventListener('click', function() {
+document.getElementById("button-auth").addEventListener("click", function () {
   if (!this.disabled) {
-    window.location.href = '/items';
+    window.location.href = "/items";
   }
 });
