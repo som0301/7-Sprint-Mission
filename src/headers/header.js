9212db6634e545c17d7b5cce8b39e021fdcab250
gayeong <gy2039587@naver.com>
2024-05-24 21:04:15 +0900

    header 수정

--- src/headers/header.js
+++ src/headers/header.js
@@ -1,20 +1,20 @@
-import logo from '../images/logo.svg';
-import './header.css';
+import logo from "../images/logo.svg";
+import "./header.css";
 
 function header() {
+  const currentPage = "/items";
+
   return (
     <header>
-
       <h1>
-      <img src={logo} alt='로고' width={'153px'} height={'51px'} />
-      <div className='header-tab'>
-        <h2>자유게시판</h2>
-        <h2>중고마켓</h2>
-      </div>
+        <img src={logo} alt="로고" width={"153px"} height={"51px"} />
+        <div className="header-tab">
+          <h2>자유게시판</h2>
+          <h2 className={currentPage === "/items" ? "activeLink" : "inactiveLink"}>중고마켓</h2>
+        </div>
       </h1>
-      
-      <button className='login'>로그인</button>
-      </header>
+      <button className="login">로그인</button>
+    </header>
   );
 }
 
