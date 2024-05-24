c87b24cd364a6d4db426c4bd739e38c7bd740a6d
KingNono1030 <hoejy0821@gmail.com>
2024-05-25 00:41:40 +0900

    feat: format price in ko-KR

--- src/ProductList.js
+++ src/ProductList.js
@@ -3,11 +3,15 @@ import './css/product-list.css';
 
 function ProductListItem({ item }) {
   const { images, price, favoriteCount, name } = item;
+  const formattedPrice = (function formatNumber(price) {
+    return new Intl.NumberFormat('ko-KR').format(price);
+  })(price);
+
   return (
     <>
       <img className="product__image" src={images} alt={name} />
       <h4 className="product__name">{name}</h4>
-      <span className="product__price">{price}원</span>
+      <span className="product__price">{formattedPrice}원</span>
       <span className="product__favorite">
         <img className="heart-symbol" src={heartImg} alt="하트" />
         {favoriteCount}
