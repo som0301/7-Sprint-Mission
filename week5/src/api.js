2b5082e59324fddc1c91c2864d4bac7bef0ca8ba
dyk0224 <88474141+dyk0224@users.noreply.github.com>
2024-05-24 23:21:58 +0900

    Delete week5 directory

--- week5/src/api.js
+++
@@ -1,8 +0,0 @@
-export async function getLists() {
-  const response = await fetch(
-    "https://panda-market-api.vercel.app/products?page=1&pageSize=10&orderBy=recent"
-  );
-  const body = await response.json();
-
-  return body;
-}
--- week5/src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-import "./Button.css";
-
-function Button({ className = "small", children = "" }) {
-  const classNames = `Button ${className}`;
-  return <button className={classNames}>{children}</button>;
-}
-
-export default Button;
