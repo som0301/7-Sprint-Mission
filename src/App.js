715c497c8d39035ee9a00788dea3cb95621a60fe
gayeong <gy2039587@naver.com>
2024-05-24 20:21:35 +0900

    header 작성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "../src/headers/header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div>베스트 상품</div>
+      <div>전체 상품</div>
     </div>
   );
 }
---
+++ src/headers/header.js
@@ -0,0 +1,21 @@
+import logo from '../images/logo.svg';
+import './header.css';
+
+function header() {
+  return (
+    <header>
+
+      <h1>
+      <img src={logo} alt='로고' width={'153px'} height={'51px'} />
+      <div className='header-tab'>
+        <h2>자유게시판</h2>
+        <h2>중고마켓</h2>
+      </div>
+      </h1>
+      
+      <button className='login'>로그인</button>
+      </header>
+  );
+}
+
+export default header;
