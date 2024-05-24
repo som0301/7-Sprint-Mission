9c691586a52ddcc824d11f43e453b243953b68b7
Dangchan Kim <kdc5847@naver.com>
2024-05-24 22:35:00 +0900

    BestProductList, BestProductItem 구현

--- src/pages/Items/BestProductItem.jsx
+++ src/pages/Items/BestProductItem.jsx
@@ -4,12 +4,12 @@ import heartIcon from '../../assets/heartIcon.svg';
 const BestProductItem = ({ imgUrl, name, price, favoriteCount }) => {
   return (
     <div>
-      <img src="#" alt="아이템이미지" className="item-img" />
-      <h3 className="item-name">title</h3>
-      <div className="item-price">price</div>
+      <img src={imgUrl} alt="아이템이미지" className="item-img" />
+      <h3 className="item-name">{name}</h3>
+      <div className="item-price">{price}</div>
       <div className="item-favorite">
         <img src={heartIcon} alt="좋아요아이콘" />
-        <span>800</span>
+        <span>{favoriteCount}</span>
       </div>
     </div>
   );
--- src/pages/Items/BestProductList.jsx
+++ src/pages/Items/BestProductList.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BestProductItem from './BestProductItem';
 
 const BestProductList = () => {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      'https://panda-market-api.vercel.app/products?page=1&pageSize=4&orderBy=favorite'
+    )
+      .then((response) => response.json())
+      .then((result) => setProduct(result.list));
+  }, []);
+
+  console.log('ddd', product);
   return (
     <section>
       <h2>베스트 상품</h2>
       <ul className="item-list">
-        <li>
-          <BestProductItem />
-        </li>
-        <li>
-          <BestProductItem />
-        </li>
-        <li>
-          <BestProductItem />
-        </li>
-        <li>
-          <BestProductItem />
-        </li>
+        {product.map((item) => (
+          <li key={item.id}>
+            <BestProductItem
+              imgUrl={item.images[0]}
+              name={item.name}
+              price={item.price}
+              favoriteCount={item.favoriteCount}
+            />
+          </li>
+        ))}
       </ul>
     </section>
   );
