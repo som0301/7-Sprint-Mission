1e00cd1dc2a7d9bf1ff58c191ea85ad7b91dc33f
KingNono1030 <hoejy0821@gmail.com>
2024-05-26 01:17:48 +0900

    refactor: handle isMobile and isTablet states as one state named deviceType

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import './css/global.css';
 import './css/App.css';
 
 function App() {
+  const INITIAL_DEVICETYPE = {
+    isMobile: true,
+    isTablet: false,
+  };
   const [favoriteItems, setFavoriteItems] = useState([]);
   const [allItems, setAllItems] = useState([]);
   const [order, setOrder] = useState('recent');
@@ -16,8 +20,7 @@ function App() {
   const [search, setSearch] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [productsError, setProductsError] = useState(null);
-  const [isMobile, setIsMobile] = useState(true);
-  const [isTablet, setIsTablet] = useState(false);
+  const [deviceType, setDeviceType] = useState(INITIAL_DEVICETYPE);
   const [isInitialized, setIsInitialized] = useState(false);
 
   useEffect(() => {
@@ -27,18 +30,28 @@ function App() {
     );
 
     const handleMobileChange = (e) => {
-      setIsMobile(e.matches);
+      console.log('isMobile: ' + e.matches);
+      const nextIsMobile = e.matches;
+      setDeviceType((prevType) => ({
+        ...prevType,
+        isMobile: nextIsMobile,
+      }));
     };
 
     const handleTabletChange = (e) => {
-      setIsTablet(e.matches);
+      console.log('isTablet: ' + e.matches);
+      const nextIsTablet = e.matches;
+      setDeviceType((prevType) => ({
+        ...prevType,
+        isTablet: nextIsTablet,
+      }));
     };
 
     mobileMediaQuery.addListener(handleMobileChange);
     tabletMediaQuery.addListener(handleTabletChange);
 
-    setIsMobile(mobileMediaQuery.matches);
-    setIsTablet(tabletMediaQuery.matches);
+    handleMobileChange(mobileMediaQuery);
+    handleTabletChange(tabletMediaQuery);
     setIsInitialized(true);
 
     return () => {
@@ -81,24 +94,25 @@ function App() {
 
   useEffect(() => {
     if (!isInitialized) return;
-
+    const { isMobile, isTablet } = deviceType;
     const responsivePageSize = isMobile ? 1 : isTablet ? 2 : 4;
     loadFavoriteItems({
       order: 'favorite',
       pageSize: responsivePageSize,
     });
-  }, [isMobile, isTablet, isInitialized]);
+  }, [deviceType, isInitialized]);
 
   useEffect(() => {
     if (!isInitialized) return;
 
+    const { isMobile, isTablet } = deviceType;
     const responsivePageSize = isMobile ? 4 : isTablet ? 6 : 10;
     loadAllItems({ order, page, pageSize: responsivePageSize });
-  }, [order, page, isMobile, isTablet, isInitialized]);
+  }, [order, page, deviceType, isInitialized]);
 
   return (
     <>
-      <NavBar isMobile={isMobile} />
+      <NavBar isMobile={false} />
       <FavoriteProductSection items={favoriteItems} />
       <AllProductSection
         items={allItems}
