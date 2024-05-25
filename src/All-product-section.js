219154b3126bf871cbc244104a45e3029de7ac93
KingNono1030 <hoejy0821@gmail.com>
2024-05-26 03:24:55 +0900

    refactor: refactor product section styles

--- src/All-product-section.js
+++ src/All-product-section.js
@@ -25,37 +25,39 @@ export default function AllProductSection({ onClick, items, isLoading }) {
       <div className="product-section__header">
         <h2 className="product-section__header-text">판매 중인 상품</h2>
         <CommonButton onClick={handleLinkClick}>상품 등록하기</CommonButton>
-        <input
-          className="product-section__header__input"
-          placeholder="🔍 검색할 상품을 입력해주세요"
-        />
-        <button
-          onClick={handleDropdownClick}
-          className="product-section__header__drop-down"
-          type="button"
-        >
-          <img src={dropdownImg} alt="드롭다운 버튼" />
-          {openDropdown && (
-            <div className="drop-down__list__orderby">
-              <button
-                onClick={onOrder}
-                id="recent"
-                className="orderby__recent"
-                disabled={isLoading}
-              >
-                최신순
-              </button>
-              <button
-                onClick={onOrder}
-                id="favorite"
-                className="orderby__favorite"
-                disabled={isLoading}
-              >
-                좋아요순
-              </button>
-            </div>
-          )}
-        </button>
+        <div className="product-section__header__container">
+          <input
+            className="product-section__header__input"
+            placeholder="🔍 검색할 상품을 입력해주세요"
+          />
+          <button
+            onClick={handleDropdownClick}
+            className="product-section__header__drop-down"
+            type="button"
+          >
+            <img src={dropdownImg} alt="드롭다운 버튼" />
+            {openDropdown && (
+              <div className="drop-down__list__orderby">
+                <button
+                  onClick={onOrder}
+                  id="recent"
+                  className="orderby__recent"
+                  disabled={isLoading}
+                >
+                  최신순
+                </button>
+                <button
+                  onClick={onOrder}
+                  id="favorite"
+                  className="orderby__favorite"
+                  disabled={isLoading}
+                >
+                  좋아요순
+                </button>
+              </div>
+            )}
+          </button>
+        </div>
       </div>
       <ProductList className="product-list__all" items={items} />
     </section>
