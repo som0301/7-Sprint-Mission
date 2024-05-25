db0d5af0d6a14498bac2cd41a06fc568f718c003
Dangchan Kim <kdc5847@naver.com>
2024-05-26 01:11:38 +0900

    feat: add new route for Community page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Items from './pages/Items/index';
+import Community from './pages/Community/Community';
 import './App.css';
 import Header from './Header';
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/community" element={<Community />} />
         <Route path="/items" element={<Items />} />
       </Routes>
     </>
