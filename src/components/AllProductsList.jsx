4834c6739b08be0ae8ae968168572a4197c979b3
minkyung <mxkxx@naver.com>
2024-05-25 16:10:40 +0900

    💄 Docs: 전체 상품 부분 모바일 버전 ui 수정

--- src/components/AllProductsList.jsx
+++ src/components/AllProductsList.jsx
@@ -6,6 +6,13 @@ import Pagination from "./Pagination";
 import ProductItem from "./ProductItem";
 import Search from "./Search";
 
+import {
+  AllProductsListHeader,
+  MobileAllProductsListHeader,
+} from "./AllProductsListHeader";
+
+import { useResponsiveApi } from "/src/Responsive";
+
 function AllProductsListItem({ product, className }) {
   const [order, setOrder] = useState("recent");
 
@@ -30,6 +37,7 @@ function AllProductsList({
   setPage,
   page,
 }) {
+  const { isMobile } = useResponsiveApi();
   const navigate = useNavigate();
   const handleAddProduct = () => {
     navigate("/additem");
@@ -37,20 +45,19 @@ function AllProductsList({
 
   return (
     <div className={className}>
-      <div className="all-products-header">
-        <h2 className="products-list-title">전체 상품</h2>
-        <div className="search-order-div">
-          {/* .search-order-div 이 div에 마땅한 클래스네임을 모르겠다..*/}
-          <Search className="input--color" />
-          <Button
-            onClick={handleAddProduct}
-            className="btn btn-product-register"
-          >
-            상품 등록하기
-          </Button>
-          <Dropdown order={order} setOrder={setOrder} />
-        </div>
-      </div>
+      {isMobile ? (
+        <MobileAllProductsListHeader
+          onClick={handleAddProduct}
+          order={order}
+          setOrder={setOrder}
+        />
+      ) : (
+        <AllProductsListHeader
+          onClick={handleAddProduct}
+          order={order}
+          setOrder={setOrder}
+        />
+      )}
 
       <div className="row">
         {products.map((product) => {
