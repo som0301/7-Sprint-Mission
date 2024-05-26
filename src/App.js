4df53252225bc67fbbb4b4c2c58500d9c352d8ab
ddol9 <drddol9@gmail.com>
2024-05-27 01:16:17 +0900

    feat Add NavBar.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import { getItems } from "./api";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getItems().then((data) => {
+      setItems(data);
+    });
+  }
+  , []);
+
+
   return (
     <>
       <NavBar />
+      
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,22 +5,26 @@ import logoImg from "./images/logo-home.png";
 
 function NavBar() {
   return (
-    <div>
-      <Link to='/'>
-        <img src={logoImg} alt='logo' />
-      </Link>
-      <ul>
-        <li>
-          <Link to='/'>자유게시판</Link>
-        </li>
-        <li>
-          <Link to='/about'>중고마켓</Link>
-        </li>
-      </ul>
-      <div className='button'>
-        <Link to='/signin'>로그인</Link>
+    <>
+      <div className='navbar'>
+        <div className='navbar-logo'>
+          <Link to='/'>
+            <img src={logoImg} alt='logo' />
+          </Link>
+        </div>
+        <ul className='navbar-links'>
+          <li>
+            <Link to='/'>자유게시판</Link>
+          </li>
+          <li>
+            <Link to='/about'>중고마켓</Link>
+          </li>
+        </ul>
+        <div className='button'>
+          <Link to='/signin'>로그인</Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
