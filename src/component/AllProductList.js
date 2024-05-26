1506d16aa0e0e7c659ce1e529be71b14e131bad9
홍준기 <hongjungi@hongjungiui-noteubug.local>
2024-05-26 19:21:35 +0900

    전체적인 틀 완성, 전체 상품 정렬 기능 추가

--- src/component/AllProductList.js
+++ src/component/AllProductList.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import ProductItem from "./ProductItem";
 
 function AllProductListItem({ product, className }) {
@@ -13,12 +14,43 @@ function AllProductListItem({ product, className }) {
 }
 
 function AllproductsList({ products, className }) {
+  const [sortOption, setSortOption] = useState("recent");
+
+  const handleSortChange = (e) => {
+    setSortOption(e.target.value);
+  };
+
+  const SortedProducts = [...products].sort((a, b) => {
+    if (sortOption === "favorite") {
+      return b.favoriteCount - a.favoriteCount;
+    } else {
+      return new Date(b.createdAt) - new Date(a.createdAt);
+    }
+  });
+
   return (
     <div className={className}>
-      <h2 className="products-list-title">전체 상품</h2>
+      <div className="all-products-Navbar">
+        <h2 className="products-list-title">전체 상품</h2>
+        <div className="all-products-Nav">
+          <input
+            className="search"
+            placeholder="검색할 상품을 입력해  주세요."
+          />
+          <button className="small-button">상품 등록하기</button>
+          <select
+            className="drop-down"
+            value={sortOption}
+            onChange={handleSortChange}
+          >
+            <option value="recent">최신순</option>
+            <option value="favorite">좋아요순</option>
+          </select>
+        </div>
+      </div>
       <div className="row-list">
-        {products.map((product) => {
-          return <AllProductListItem product={product} />;
+        {SortedProducts.map((product) => {
+          return <AllProductListItem key={product.id} product={product} />;
         })}
       </div>
     </div>
