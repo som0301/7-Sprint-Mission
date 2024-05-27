b7d708ba96afee4e80fd0a3415f5bce14f2d89e6
hbs0133 <hbs0133@naver.com>
2024-05-27 09:14:22 +0900

    ItemCard 가격표수정

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./assets/styles/reset.css";
 import "./assets/styles/common.css";
 import SecondHandMarket from "./pages/SecondHandMarket/SecondHandMarket";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/items" replace />} />
         <Route path="/items" element={<SecondHandMarket />}></Route>
       </Routes>
     </BrowserRouter>
--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -14,7 +14,7 @@ const ItemCard = ({ item, imageSize }) => {
         alt={`${name} 이미지`}
       />
       <h2 className="item-card-name">{name}</h2>
-      <h2 className="item-card-price">{price}</h2>
+      <h2 className="item-card-price">{price.toLocaleString()}원</h2>
       <h3 className="item-card-favorite-count">
         <img src={favoriteIcon} alt="좋아요기능 하트모양 아이콘" />
         {favoriteCount}
