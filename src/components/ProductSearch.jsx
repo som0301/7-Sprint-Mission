b9a6ff3f82836ea0e4bb9e9046e48cb22df82ea9
서미영 <seomiyoung3998@gmail.com>
2024-05-27 20:09:52 +0900

    레이아웃 마무리

--- src/components/ProductSearch.jsx
+++ src/components/ProductSearch.jsx
@@ -3,6 +3,8 @@ import "../styles/global.css";
 import "./ProductPage.css";
 import { Link } from "react-router-dom";
 import searchImg from "../assets/images/icons/ic_search.svg";
+import useDeviceType from "../hooks/useDeviceType";
+import dropdownImg from "../assets/images/icons/ic_sort.svg";
 
 function ProductSearch({ onOptionChange }) {
   const [selectedOption, setSelectedOption] = useState("최신순");
@@ -11,6 +13,12 @@ function ProductSearch({ onOptionChange }) {
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
+
+  const { getDeviceType } = useDeviceType();
+
+  const deviceType = getDeviceType();
+
+  console.log(deviceType);
 
   const handleOptionClick = (option) => {
     setSelectedOption(option);
@@ -35,8 +43,14 @@ function ProductSearch({ onOptionChange }) {
         </button>
         <div className="dropdown">
           <button className="dropdown-toggle" onClick={toggleDropdown}>
-            {selectedOption}
-            <span className="arrow">{isOpen ? "▲" : "▼"}</span>
+            {deviceType !== "mobile" ? selectedOption : null}
+
+            {deviceType !== "mobile" && (
+              <span className="arrow">{isOpen ? "▲" : "▼"}</span>
+            )}
+            {deviceType === "mobile" && (
+              <img id="dropdown-img" src={dropdownImg} alt="드롭다운 버튼" />
+            )}
           </button>
           {isOpen && (
             <ul className="dropdown-menu">
