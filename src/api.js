3ac5a4e43361d60f8a34e4980c6cd57c1bc9dfdb
장아영 <jang-ayoung@jang-ayeong-ui-MacBookPro.local>
2024-05-30 16:14:52 +0900

    api.js 수정

--- src/api.js
+++ src/api.js
@@ -1,19 +1,36 @@
-export async function getItems({
-  orderBy = "favorite",
+// export async function getItems({
+//   orderBy = "favorite",
+//   page = 1,
+//   pageSize = 4,
+// }) {
+//   const query = `page=${page}&pageSize=${pageSize}&orderBy=${orderBy}`;
+//   const response = await fetch(
+//     `https://panda-market-api.vercel.app/products?${query}`,
+//     {
+//       // headers: {
+//       //   "Content-Type": "application/json",
+//       //   Accept: "application/json",
+//       // },
+//       // method: "GET",
+//     }
+//   );
+//   const body = await response.json();
+//   return body;
+// }
+
+export default async function getItems({
   page = 1,
-  pageSize = 4,
+  pageSize,
+  orderBy = "recent",
+  search = "",
 }) {
   const query = `page=${page}&pageSize=${pageSize}&orderBy=${orderBy}`;
   const response = await fetch(
-    `https://panda-market-api.vercel.app/products?${query}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      method: "GET",
-    }
+    `https://panda-market-api.vercel.app/products?${query}`
   );
+  if (!response) {
+    throw new Error("데이터를 불러오는데 실패했습니다");
+  }
   const body = await response.json();
   return body;
 }
