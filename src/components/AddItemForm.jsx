60f0a4badadebfb90d568fcc923c7d3a1fe9dedb
minkyung <mxkxx@naver.com>
2024-05-30 19:01:09 +0900

    ✨ Feat: 태그 추가 기능 구현

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,11 +1,13 @@
-import Input from './Input';
+import { Input, Tag } from './Input';
 import { styled } from 'styled-components';
 import Button from './common/Button';
+import { useState } from 'react';
 
 const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
   gap: 24px;
+  margin-top: 24px;
 `;
 
 const StyledFormHeader = styled.div`
@@ -19,48 +21,121 @@ const FormHeaderTitle = styled.h2`
 `;
 
 function AddItemForm() {
+  const [tags, setTags] = useState([]);
+  const [values, setValues] = useState({
+    name: '',
+    description: '',
+    price: '',
+    tags: { tags },
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleNumberChange = (e) => {
+    const { name } = e.target;
+    const value = Number(e.target.value);
+    if (isNaN(value)) return;
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleTagChange = (e) => {
+    const { value } = e.target;
+    if (value.trim() === '') return;
+    if (tags.length === 10) return;
+    setTags((prevTags) => [...new Set([...prevTags, value.trim()])]);
+    setValues((prevValues) => ({ ...prevValues, ['tags']: tags }));
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.code !== 'Enter') return;
+    e.preventDefault();
+  };
+
+  const handleKeyUp = (e) => {
+    if (e.code === 'Enter') {
+      e.preventDefault();
+      handleTagChange(e);
+      e.target.value = '';
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
+  const TagContainer = styled.div`
+    display: flex;
+    gap: 12px;
+    margin-top: 12px;
+  `;
+
   return (
-    <form>
+    <StyledForm onSubmit={handleSubmit}>
       <StyledFormHeader>
         <FormHeaderTitle>상품 등록하기</FormHeaderTitle>
-        <Button className='btn btn-small' disabled>
+        <Button type='submit' className='btn btn-small'>
           등록
         </Button>
       </StyledFormHeader>
       <Input
-        htmlFor='ProductName'
+        name='name'
+        value={values.name}
         id='ProductName'
         type='text'
         placeholder='상품명을 입력해주세요'
+        onChange={handleChange}
       >
         상품명
       </Input>
       <Input
-        htmlFor='ProductDescription'
+        name='description'
+        value={values.description}
         id='ProductDescription'
         type='textarea'
         placeholder='상품 소개를 입력해주세요'
-        height='200px'
+        onChange={handleChange}
       >
         상품 소개
       </Input>
       <Input
-        htmlFor='ProductPrice'
+        name='price'
+        value={values.price}
         id='ProductPrice'
         type='text'
         placeholder='판매 가격을 입력해주세요'
+        onChange={handleNumberChange}
       >
         판매 가격
       </Input>
-      <Input
-        htmlFor='ProductTag'
-        id='ProductTag'
-        type='text'
-        placeholder='태그를 입력해주세요'
-      >
-        태그
-      </Input>
-    </form>
+      <div>
+        <Input
+          id='ProductTag'
+          name='tags'
+          type='text'
+          placeholder='태그를 입력해주세요 (최대 10개)'
+          onKeyUp={handleKeyUp}
+          onKeyDown={handleKeyDown}
+        >
+          태그
+        </Input>
+        <TagContainer>
+          {tags.map((tag, index) => (
+            //   <StyledTag key={index}>{tag}</StyledTag>
+            <Tag key={index}>{tag}</Tag>
+          ))}
+        </TagContainer>
+      </div>
+    </StyledForm>
   );
 }
 
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,14 @@
 import '/src/styles/Color.css';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import iconTagCancel from '/src/assets/ic_tag_cancel.svg';
 
 const WEIGHTS = {};
+
+const TextStyle = css`
+  font-size: 16px;
+  font-weight: 400;
+  color: var(--gray-800);
+`;
 
 export const StyledInputContainer = styled.div`
   display: flex;
@@ -15,13 +22,12 @@ export const StyledLabel = styled.label`
   color: var(--gray-800);
 `;
 
-export const StyledInput = styled.input`
+const InputStyle = css`
   width: 100%;
   background-color: var(--gray-100);
   position: relative;
 
-  font-size: 16px;
-  font-weight: 400;
+  ${TextStyle}
 
   padding: 16px 24px;
   border: none;
@@ -34,14 +40,82 @@ export const StyledInput = styled.input`
     top: 16px;
     left: 24px;
   }
-  height: ${({ type }) => (type === 'textarea' ? `200px` : ``)};
 `;
 
-export default function Input({ htmlFor, type, id, placeholder, children }) {
+const StyledInput = styled.input`
+  ${InputStyle}
+`;
+
+const StyledTextArea = styled.textarea`
+  resize: none;
+  ${InputStyle}
+`;
+
+const StyledTag = styled.div`
+  display: inline-flex;
+  background-color: var(--gray-50);
+  padding: 12px 12px 12px 16px;
+  border-radius: 26px;
+
+  ${TextStyle}
+
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+`;
+
+const ImgButton = styled.button`
+  margin: 0;
+  padding: 0;
+`;
+
+export function Tag({ children }) {
+  return (
+    <StyledTag>
+      <span>{children}</span>
+      {/* <button type='button'>
+        
+      </button> */}
+      <img src={iconTagCancel} />
+    </StyledTag>
+  );
+}
+
+export function Input({
+  name,
+  value,
+  type,
+  id,
+  placeholder,
+  children,
+  onChange,
+  onKeyDown,
+  onKeyUp,
+}) {
   return (
     <StyledInputContainer>
-      <StyledLabel htmlFor={htmlFor}>{children}</StyledLabel>
-      <StyledInput type={type} id={id} placeholder={placeholder} />
+      <StyledLabel htmlFor={id}>{children}</StyledLabel>
+      {type === 'textarea' ? (
+        <StyledTextArea
+          name={name}
+          value={value}
+          id={id}
+          placeholder={placeholder}
+          onChange={onChange}
+          rows='5'
+        />
+      ) : (
+        <StyledInput
+          name={name}
+          value={value}
+          type={type}
+          id={id}
+          placeholder={placeholder}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          onKeyUp={onKeyUp}
+        />
+      )}
     </StyledInputContainer>
   );
 }
