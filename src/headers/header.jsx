3fcb26e74ff95a2bf31326a51e372dfc283b2463
gayeong <gy2039587@naver.com>
2024-05-30 17:25:43 +0900

    반응형 완성

--- src/headers/header.jsx
+++ src/headers/header.jsx
@@ -1,22 +1,58 @@
-import { Link, NavLink} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../images/logo.svg";
 import "./header.css";
 
 function getLinkstyle({ isActive }) {
-  return { color: isActive ? "#3692FF" : "#4b5563" }
+  return { color: isActive ? "#3692FF" : "#4b5563" };
 }
 
 function header() {
   return (
     <header>
-      <h1>
-        <Link to="/" className="link"><img src={logo} alt="로고" width={"153px"} height={"51px"} /></Link>
-        <div className="header-tab">
-          <h2><NavLink to="/board" className="link" style={getLinkstyle}>자유게시판</NavLink></h2>
-          <h2><NavLink to="/items" className="link" style={getLinkstyle}>중고마켓</NavLink></h2>
+      {window.innerWidth >= 768 && (
+        <div className="header-top">
+          <h1>
+            <Link to="/" className="link">
+              <img src={logo} alt="로고" width={"153px"} height={"51px"} />
+            </Link>
+            <div className="header-tab">
+              <h2>
+                <NavLink to="/board" className="link" style={getLinkstyle}>
+                  자유게시판
+                </NavLink>
+              </h2>
+              <h2>
+                <NavLink to="/items" className="link" style={getLinkstyle}>
+                  중고마켓
+                </NavLink>
+              </h2>
+            </div>
+          </h1>
+          <button className="login">로그인</button>
         </div>
-      </h1>
-      <button className="login">로그인</button>
+      )}
+      {window.innerWidth >= 375 && window.innerWidth <= 767 && (
+        <div className="header-top">
+          <h1>
+            <Link to="/" className="link">
+              <div className="title">판다마켓</div>
+            </Link>
+            <div className="header-tab">
+              <h2>
+                <NavLink to="/board" className="link" style={getLinkstyle}>
+                  자유게시판
+                </NavLink>
+              </h2>
+              <h2>
+                <NavLink to="/items" className="link" style={getLinkstyle}>
+                  중고마켓
+                </NavLink>
+              </h2>
+            </div>
+          </h1>
+          <button className="login">로그인</button>
+        </div>
+      )}
     </header>
   );
 }
