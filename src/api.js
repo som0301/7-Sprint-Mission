f368f1df558de2271d672498d95e3fe0f59e41ff
ddol9 <drddol9@gmail.com>
2024-05-31 16:25:10 +0900

    feat: Add api.js catch error

--- src/api.js
+++ src/api.js
@@ -1,7 +1,13 @@
+const BASE_URL = "https://panda-market-api.vercel.app";
+
 export async function getItems({ page = 1, pageSize = 4, orderBy }) {
-  const BASE_URL = "https://panda-market-api.vercel.app";
   const query = `/products?page=${page}&pageSize=${pageSize}&orderBy=${orderBy}`;
-  const response = await fetch(`${BASE_URL}${query}`);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(`${BASE_URL}${query}`);
+    const data = await response.json();
+    return data;
+  } catch(error) {
+    console.error('Error fetching items', error);
+    return [];
+  }
 }
