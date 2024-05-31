b955da7a9cb7fcff8a75757f6544528a9bf30c70
dyk0224 <88474141+dyk0224@users.noreply.github.com>
2024-06-01 03:22:38 +0900

    Create utils.js

---
+++ src/modules/js/utils.js
@@ -0,0 +1,25 @@
+export function getDeviceType(width) {
+  if (width >= 1200) return "desktop";
+  if (width >= 768) return "tablet";
+
+  return "mobile";
+}
+
+export function attachSubjectParticle(name) {
+  const lastWord = hasFinalConsonant(name);
+  const result = name + (lastWord === 0 ? "가" : "이");
+  return result;
+}
+
+export function attachObjectParticle(name) {
+  const lastWord = hasFinalConsonant(name);
+  const result = name + (lastWord === 0 ? "를" : "을");
+  return result;
+}
+
+function hasFinalConsonant(name) {
+  const charCode = name.charCodeAt(name.length - 1);
+  const consonantCode = (charCode - 44032) % 28;
+
+  return consonantCode;
+}
