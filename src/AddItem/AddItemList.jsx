aad98f4d272d649e309b5d1ab7f68cdd3f74635a
gayeong <gy2039587@naver.com>
2024-06-01 22:12:43 +0900

    tag 완성

--- src/AddItem/AddItemList.jsx
+++ src/AddItem/AddItemList.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import AddItemHeader from "../AddItem/AddItemHeader";
 import "./AddItemList.css";
 import FileInputImage from "./fileInputImage";
+import deleteTag from "../images/deleteTag.svg";
 
 function formatNumber(number) {
   return new Intl.NumberFormat("ko-KR").format(number);
@@ -9,39 +10,68 @@ function formatNumber(number) {
 
 function AddItemList() {
   const [isDisabled, setIsDisabled] = useState(true);
-  const [formaData, setFormData] = useState({
+  const [formData, setFormData] = useState({
     productName: "",
     productIntroduce: "",
     productPrice: "",
-    productTag: "",
+    productTag: [],
   });
+  const [productTagInput, setProductTagInput] = useState("");
 
   const handleInput = (e) => {
     const { name, value } = e.target;
     if (name === "productPrice" && (isNaN(Number(value)) || value < 0)) {
       return;
     }
-    let updatedValue=value;
+    let updatedValue = value;
 
-    if(name==="productPrice"){
-     updatedValue=formatNumber(value);
+    if (name === "productPrice") {
+      updatedValue = formatNumber(value);
     }
-
     setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: updatedValue,
     }));
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const handleTagInputChange = (e) => {
+    setProductTagInput(e.target.value);
+  };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      const newTag = productTagInput.trim();
+
+      if (newTag !== "") {
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          productTag: [...prevFormData.productTag, newTag],
+        }));
+        setProductTagInput("");
+      }
+    }
+  };
+
+  const handleRemoveTag = (indexToRemove) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      productTag: prevFormData.productTag.filter(
+        (_, index) => index !== indexToRemove
+      ),
+    }));
+  };
 
   useEffect(() => {
     const { productName, productIntroduce, productPrice, productTag } =
-      formaData;
+      formData;
     if (productName && productIntroduce && productPrice && productTag) {
       setIsDisabled(false);
     } else {
       setIsDisabled(true);
     }
-  }, [formaData]);
+  }, [formData]);
 
   return (
     <div>
@@ -53,6 +83,7 @@ function AddItemList() {
           className={`add-button ${isDisabled ? "disabled" : ""}`}
           type="submit"
           disabled={isDisabled}
+          onClick={handleSubmit}
         >
           등록
         </button>
@@ -67,7 +98,7 @@ function AddItemList() {
           </label>
           <input
             name="productName"
-            value={formaData.productName}
+            value={formData.productName}
             className="add-product-input name"
             id="product-name"
             type="text"
@@ -82,7 +113,7 @@ function AddItemList() {
           </label>
           <textarea
             name="productIntroduce"
-            value={formaData.productIntroduce}
+            value={formData.productIntroduce}
             className="add-product-input introduce"
             id="product-introduce"
             type="text"
@@ -97,7 +128,7 @@ function AddItemList() {
           </label>
           <input
             name="productPrice"
-            value={formaData.productPrice}
+            value={formData.productPrice}
             className="add-product-input price"
             id="product-price"
             type="text"
@@ -112,13 +143,27 @@ function AddItemList() {
           </label>
           <input
             name="productTag"
-            value={formaData.productTag}
+            value={productTagInput}
             className="add-product-input tag"
             id="product-tag"
             type="text"
             placeholder="태그를 입력해주세요"
-            onChange={handleInput}
+            onChange={handleTagInputChange}
+            onKeyDown={handleKeyDown}
           ></input>
+          <div className="tag-container">
+            {formData.productTag.map((tag, index) => (
+              <div key={index} className="tag-container-value">
+                {tag}
+                <button
+                  className="delete-tag"
+                  onClick={() => handleRemoveTag(index)}
+                >
+                  <img src={deleteTag} alt="태그 삭제" />
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
