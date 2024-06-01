0db23aff561c471514b1323778923748946a2261
gayeong <gy2039587@naver.com>
2024-06-01 23:36:27 +0900

    반응형 완성

--- src/AddItem/AddItemList.jsx
+++ src/AddItem/AddItemList.jsx
@@ -9,6 +9,7 @@ function formatNumber(number) {
 }
 
 function AddItemList() {
+  const [windowInnerWidth, setWindowInnerWidth] = useState(window.innerWidth);
   const [isDisabled, setIsDisabled] = useState(true);
   const [formData, setFormData] = useState({
     productName: "",
@@ -74,98 +75,272 @@ function AddItemList() {
   }, [formData]);
 
   return (
-    <div>
+    <div className="all-wrapper">
       <AddItemHeader />
-
-      <div className="add-top">
-        <h2 className="register">상품 등록하기</h2>
-        <button
-          className={`add-button ${isDisabled ? "disabled" : ""}`}
-          type="submit"
-          disabled={isDisabled}
-          onClick={handleSubmit}
-        >
-          등록
-        </button>
-      </div>
-
-      <div className="add-total">
-        <FileInputImage />
-
-        <div className="add-product">
-          <label className="add-label" htmlFor="product-name">
-            상품명
-          </label>
-          <input
-            name="productName"
-            value={formData.productName}
-            className="add-product-input name"
-            id="product-name"
-            type="text"
-            placeholder="상품명을 입력해주세요"
-            onChange={handleInput}
-          ></input>
-        </div>
-
-        <div className="add-product">
-          <label className="add-label" htmlFor="product-introduce">
-            상품 소개
-          </label>
-          <textarea
-            name="productIntroduce"
-            value={formData.productIntroduce}
-            className="add-product-input introduce"
-            id="product-introduce"
-            type="text"
-            placeholder="상품 소개를 입력해주세요"
-            onChange={handleInput}
-          ></textarea>
-        </div>
-
-        <div className="add-product">
-          <label className="add-label" htmlFor="product-price">
-            판매 가격
-          </label>
-          <input
-            name="productPrice"
-            value={formData.productPrice}
-            className="add-product-input price"
-            id="product-price"
-            type="text"
-            placeholder="판매 가격을 입력해주세요"
-            onChange={handleInput}
-          ></input>
-        </div>
-
-        <div className="add-product">
-          <label className="add-label" htmlFor="product-tag">
-            태그
-          </label>
-          <input
-            name="productTag"
-            value={productTagInput}
-            className="add-product-input tag"
-            id="product-tag"
-            type="text"
-            placeholder="태그를 입력해주세요"
-            onChange={handleTagInputChange}
-            onKeyDown={handleKeyDown}
-          ></input>
-          <div className="tag-container">
-            {formData.productTag.map((tag, index) => (
-              <div key={index} className="tag-container-value">
-                {tag}
-                <button
-                  className="delete-tag"
-                  onClick={() => handleRemoveTag(index)}
-                >
-                  <img src={deleteTag} alt="태그 삭제" />
-                </button>
+      {window.innerWidth >= 1200 && (
+        <div className="wrapper">
+          <div className="add-top">
+            <h2 className="register">상품 등록하기</h2>
+            <button
+              className={`add-button ${isDisabled ? "disabled" : ""}`}
+              type="submit"
+              disabled={isDisabled}
+              onClick={handleSubmit}
+            >
+              등록
+            </button>
+          </div>
+          <div className="add-total">
+            <FileInputImage />
+            <div className="add-product">
+              <label className="add-label" htmlFor="product-name">
+                상품명
+              </label>
+              <input
+                name="productName"
+                value={formData.productName}
+                className="add-product-input name"
+                id="product-name"
+                type="text"
+                placeholder="상품명을 입력해주세요"
+                onChange={handleInput}
+              ></input>
+            </div>
+            <div className="add-product">
+              <label className="add-label" htmlFor="product-introduce">
+                상품 소개
+              </label>
+              <textarea
+                name="productIntroduce"
+                value={formData.productIntroduce}
+                className="add-product-input introduce"
+                id="product-introduce"
+                type="text"
+                placeholder="상품 소개를 입력해주세요"
+                onChange={handleInput}
+              ></textarea>
+            </div>
+            <div className="add-product">
+              <label className="add-label" htmlFor="product-price">
+                판매 가격
+              </label>
+              <input
+                name="productPrice"
+                value={formData.productPrice}
+                className="add-product-input price"
+                id="product-price"
+                type="text"
+                placeholder="판매 가격을 입력해주세요"
+                onChange={handleInput}
+              ></input>
+            </div>
+            <div className="add-product">
+              <label className="add-label" htmlFor="product-tag">
+                태그
+              </label>
+              <input
+                name="productTag"
+                value={productTagInput}
+                className="add-product-input tag"
+                id="product-tag"
+                type="text"
+                placeholder="태그를 입력해주세요"
+                onChange={handleTagInputChange}
+                onKeyDown={handleKeyDown}
+              ></input>
+              <div className="tag-container">
+                {formData.productTag.map((tag, index) => (
+                  <div key={index} className="tag-container-value">
+                    {tag}
+                    <button
+                      className="delete-tag"
+                      onClick={() => handleRemoveTag(index)}
+                    >
+                      <img src={deleteTag} alt="태그 삭제" />
+                    </button>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
+          </div>
+        </div>
+      )}
+      {window.innerWidth >=768 && window.innerWidth<=1199 &&(
+        <div className="wrapper">
+        <div className="add-top">
+          <h2 className="register">상품 등록하기</h2>
+          <button
+            className={`add-button ${isDisabled ? "disabled" : ""}`}
+            type="submit"
+            disabled={isDisabled}
+            onClick={handleSubmit}
+          >
+            등록
+          </button>
+        </div>
+        <div className="add-total">
+          <FileInputImage />
+          <div className="add-product">
+            <label className="add-label" htmlFor="product-name">
+              상품명
+            </label>
+            <input
+              name="productName"
+              value={formData.productName}
+              className="add-product-input name"
+              id="product-name"
+              type="text"
+              placeholder="상품명을 입력해주세요"
+              onChange={handleInput}
+            ></input>
+          </div>
+          <div className="add-product">
+            <label className="add-label" htmlFor="product-introduce">
+              상품 소개
+            </label>
+            <textarea
+              name="productIntroduce"
+              value={formData.productIntroduce}
+              className="add-product-input introduce"
+              id="product-introduce"
+              type="text"
+              placeholder="상품 소개를 입력해주세요"
+              onChange={handleInput}
+            ></textarea>
+          </div>
+          <div className="add-product">
+            <label className="add-label" htmlFor="product-price">
+              판매 가격
+            </label>
+            <input
+              name="productPrice"
+              value={formData.productPrice}
+              className="add-product-input price"
+              id="product-price"
+              type="text"
+              placeholder="판매 가격을 입력해주세요"
+              onChange={handleInput}
+            ></input>
+          </div>
+          <div className="add-product">
+            <label className="add-label" htmlFor="product-tag">
+              태그
+            </label>
+            <input
+              name="productTag"
+              value={productTagInput}
+              className="add-product-input tag"
+              id="product-tag"
+              type="text"
+              placeholder="태그를 입력해주세요"
+              onChange={handleTagInputChange}
+              onKeyDown={handleKeyDown}
+            ></input>
+            <div className="tag-container">
+              {formData.productTag.map((tag, index) => (
+                <div key={index} className="tag-container-value">
+                  {tag}
+                  <button
+                    className="delete-tag"
+                    onClick={() => handleRemoveTag(index)}
+                  >
+                    <img src={deleteTag} alt="태그 삭제" />
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
+      )}
+      {window.innerWidth>=375 && window.innerWidth<=767 &&(
+        <div className="wrapper">
+        <div className="add-top">
+          <h2 className="register">상품 등록하기</h2>
+          <button
+            className={`add-button ${isDisabled ? "disabled" : ""}`}
+            type="submit"
+            disabled={isDisabled}
+            onClick={handleSubmit}
+          >
+            등록
+          </button>
+        </div>
+        <div className="add-total">
+          <FileInputImage />
+          <div className="add-product">
+            <label className="add-label" htmlFor="product-name">
+              상품명
+            </label>
+            <input
+              name="productName"
+              value={formData.productName}
+              className="add-product-input name"
+              id="product-name"
+              type="text"
+              placeholder="상품명을 입력해주세요"
+              onChange={handleInput}
+            ></input>
+          </div>
+          <div className="add-product">
+            <label className="add-label" htmlFor="product-introduce">
+              상품 소개
+            </label>
+            <textarea
+              name="productIntroduce"
+              value={formData.productIntroduce}
+              className="add-product-input introduce"
+              id="product-introduce"
+              type="text"
+              placeholder="상품 소개를 입력해주세요"
+              onChange={handleInput}
+            ></textarea>
+          </div>
+          <div className="add-product">
+            <label className="add-label" htmlFor="product-price">
+              판매 가격
+            </label>
+            <input
+              name="productPrice"
+              value={formData.productPrice}
+              className="add-product-input price"
+              id="product-price"
+              type="text"
+              placeholder="판매 가격을 입력해주세요"
+              onChange={handleInput}
+            ></input>
+          </div>
+          <div className="add-product">
+            <label className="add-label" htmlFor="product-tag">
+              태그
+            </label>
+            <input
+              name="productTag"
+              value={productTagInput}
+              className="add-product-input tag"
+              id="product-tag"
+              type="text"
+              placeholder="태그를 입력해주세요"
+              onChange={handleTagInputChange}
+              onKeyDown={handleKeyDown}
+            ></input>
+            <div className="tag-container">
+              {formData.productTag.map((tag, index) => (
+                <div key={index} className="tag-container-value">
+                  {tag}
+                  <button
+                    className="delete-tag"
+                    onClick={() => handleRemoveTag(index)}
+                  >
+                    <img src={deleteTag} alt="태그 삭제" />
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      )}
     </div>
   );
 }
