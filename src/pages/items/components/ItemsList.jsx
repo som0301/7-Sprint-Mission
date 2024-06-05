00f87432befdf806d1a63914700e64196ee679cf
reenact11 <jaehyeon11@naver.com>
2024-06-05 20:26:06 +0900

    feat: add itemtoolbar

--- src/pages/items/components/ItemsList.jsx
+++ src/pages/items/components/ItemsList.jsx
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 import useFetchItems from 'pages/items/hooks/useFetchItems';
 import useViewport from 'pages/items/hooks/useViewport';
 import HeartIcon from 'shared/assets/image/ic_heart.svg';
+import SearchIcon from 'shared/assets/image/ic_search.svg';
+import DropDownIcon from 'shared/assets/image/ic_arrow_down.svg';
 
-const ItemWrapper = styled.div`
+const ItemContainer = styled.div`
     display: flex;
     flex-direction: column;
     row-gap: 16px;
@@ -14,10 +16,67 @@ const ItemWrapper = styled.div`
     }}}
 `;
 
+const ItemWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
 const ItemType = styled.h2`
     font-size: 20px;
     line-height: 140%;
 `;
+
+const ItemToolbar = styled.div`
+    display: flex;
+`;
+
+const SearchItem = styled.input`
+    background-color: #f3f4f6;
+    background-image: url(${SearchIcon});
+    background-position: 15px;
+    background-repeat: no-repeat;
+    border: none;
+    border-radius: 12px;
+    height: 40px;
+    padding: 9px 20px 9px 44px;
+    width: 325px;
+`;
+
+const MoveAddItem = styled.a`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-width: 133px;
+    height: 42px;
+    background: var(--blue);
+    border-radius: 8px;
+    font-weight: 700;
+    font-size: 16px;
+    color: #fff;
+
+    &:hover {
+        background: #1967d6;
+    }
+`;
+
+const SortItem = styled.button`
+    position: relative;
+    border-radius: 12px;
+    border: 1px solid var(--gray-200);
+    display: flex;
+    align-items: center;
+    gap: 24px;
+    font-size: 16px;
+`;
+
+const SortBy = styled.span`
+    font-size: 16px;
+    line-height: 24px;
+`;
+
+const DropDown = styled.img`
+width: 24px
+height: 24px`;
 
 const ItemInfo = styled.ul`
     display: flex;
@@ -105,8 +164,18 @@ function ItemsList({ type, page, pageSize, order, search }) {
     if (error) return <p>Error: {error}</p>;
 
     return (
-        <ItemWrapper deviceType={deviceType}>
-            <ItemType>{type === 'best' ? '베스트 상품' : '전체 상품'}</ItemType>
+        <ItemContainer deviceType={deviceType}>
+            <ItemWrapper>
+                <ItemType>{type === 'best' ? '베스트 상품' : '전체 상품'}</ItemType>
+                <ItemToolbar>
+                    <SearchItem placeholder="검색할 상품을 입력해주세요"></SearchItem>
+                    <MoveAddItem>상품 등록하기</MoveAddItem>
+                    <SortItem>
+                        <SortBy>최신순</SortBy>
+                        <DropDown />
+                    </SortItem>
+                </ItemToolbar>
+            </ItemWrapper>
             <ItemInfo type={type} deviceType={deviceType}>
                 {items &&
                     items.map((item) => (
@@ -121,7 +190,7 @@ function ItemsList({ type, page, pageSize, order, search }) {
                         </ItemCard>
                     ))}
             </ItemInfo>
-        </ItemWrapper>
+        </ItemContainer>
     );
 }
 
