561658e71bb74a8af474d92c732dae5655e85086
minkyung <mxkxx@naver.com>
2024-06-06 22:43:48 +0900

    ♻️  타입 기본갑 설정
    인라인 스타일 객체 제거 후 스타일 컴포넌트로 변경
    - 스프린트 6 리뷰 반영

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { FlexWrapper } from '../components/common/CommonComponents';
 import {
   StyledMain,
   StyledSignMain,
@@ -6,24 +7,17 @@ import {
 import LogoImage from '/src/components/header/LogoImage';
 import LoginForm from '/src/components/LoginForm';
 
-// 고정 스타일이라 스타일 객체로..
 function LoginPage() {
   return (
     <StyledSignMain>
       <LogoImage type='sign' />
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '24px',
-        }}
-      >
+      <FlexWrapper $col $gap='24'>
         <LoginForm />
         <div>간편로그인</div>
         <div>
           앵커 <Link to={'/signup'}>회원가입</Link>
         </div>
-      </div>
+      </FlexWrapper>
     </StyledSignMain>
   );
 }
