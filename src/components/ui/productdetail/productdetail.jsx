3f339fad9293c2e59b083b1ea8e5f61fcc0ac04d
KWsony <blacksmuff3@gmail.com>
2024-06-08 11:14:44 +0900

    feat: productdetail.jsx product comment 없을 경우 inquiry-empty 렌더링

--- src/components/ui/productdetail/productdetail.jsx
+++ src/components/ui/productdetail/productdetail.jsx
@@ -1,12 +1,14 @@
 import styles from './productdetail.module.css';
 import { classModuleName } from '../../../modules';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getProductComments } from '../../../api';
 import Tag from '../../modules/tag/tag';
 import heart from '../../../images/icons/ic_heart.svg';
+import back from '../../../images/icons/ic_back.svg';
 import Button from './../../modules/button/button';
 import Comment from '../../modules/comment/comment';
+import inquiryEmpty from '../../../images/media/Img_inquiry_empty.svg';
 
 function ProductDetail() {
   const [comments, setComments] = useState([]);
@@ -32,7 +34,7 @@ function ProductDetail() {
   const handleChange = (e) => {
     const nextValue = e.target.value;
     setCommentInput(nextValue);
-  }
+  };
 
   useEffect(() => {
     handleLoad(id);
@@ -56,23 +58,43 @@ function ProductDetail() {
               })}
             </div>
             <div className={classModuleName('favorite-count-container', styles)}>
-              <img src={heart} alt='하트 아이콘'/>
+              <img src={heart} alt="하트 아이콘" />
               <p>{favoriteCount}</p>
             </div>
           </div>
         </section>
-        <section>
+        <section className={classModuleName('body-bottom', styles)}>
           <form className={classModuleName('comment-input-form', styles)}>
             <h2>문의 하기</h2>
-            <textarea value={commentInput} onChange={handleChange} placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."></textarea>
-            <Button type='button' isDisable={!commentInput}>등록</Button>
+            <textarea
+              value={commentInput}
+              onChange={handleChange}
+              placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
+            ></textarea>
+            <div style={{ display: 'grid' }}>
+              <Button type="button" isDisable={!commentInput}>
+                등록
+              </Button>
+            </div>
           </form>
-          <div className={classModuleName('comments-container', styles)}>
-            {comments.map((comment) => {
-              return <Comment key={comment.id} comment={comment} />;
-            })}
-          </div>
-          <Button></Button>
+          {comments.length ? (
+            <div className={classModuleName('comments-container', styles)}>
+              {comments.map((comment) => {
+                return <Comment key={comment.id} comment={comment} />;
+              })}
+            </div>
+          ) : (
+            <div className={classModuleName('inquiry-empty-container', styles)}>
+              <img src={inquiryEmpty} alt="수화기를 들고 궁금해하는 판다" />
+              <p>아직 문의가 없습니다.</p>
+            </div>
+          )}
+          <Link to="/items">
+            <Button inlineStyle={{ borderRadius: '40px', padding: '12px 38px' }}>
+              목록으로 돌아가기
+              <img src={back} alt="돌아가기" />
+            </Button>
+          </Link>
         </section>
       </div>
     </div>
