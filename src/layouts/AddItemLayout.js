9cfa65e0e710954ad0a1c422a7cb73bca848abda
JiinSeok <seokjiin1073@gmail.com>
2024-06-09 22:24:11 +0900

    refactor: styled componets로 변경

--- src/layouts/AddItemLayout.js
+++ src/layouts/AddItemLayout.js
@@ -1,1 +1,9 @@
-export default function AddItemLayout() {}
+import { Outlet } from 'react-router-dom';
+
+export default function AddItemLayout() {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+}
