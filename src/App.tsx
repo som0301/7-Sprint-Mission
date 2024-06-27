b7af78cac9c091e993806b0aee3c863a2342911c
ddol9 <drddol9@gmail.com>
2024-06-27 16:56:27 +0900

    start ts

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import MarketPage from "./pages/MarketPage/MarketPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <MarketPage />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/MarketPage/MarketPage.tsx
@@ -0,0 +1,11 @@
+import PrintBestItems from "./components/PrintBestItems";
+
+const MarketPage: React.FC = () => {
+  return (
+    <div>
+      <PrintBestItems />
+    </div>
+  );
+};
+
+export default MarketPage;
