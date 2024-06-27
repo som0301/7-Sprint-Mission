33f5c668db99d3125e1d92b8a2d39b3c659251f4
ddol9 <drddol9@gmail.com>
2024-06-27 20:18:09 +0900

    feat: Add AddItemPage

---
+++ src/pages/AddItemPage/AddItemPage.tsx
@@ -0,0 +1,7 @@
+import AddItemForm from "./components/AddItemForm";
+
+function AddItemPage() {
+  return <AddItemForm />;
+}
+
+export default AddItemPage;
---
+++ src/pages/AddItemPage/components/FileInput.tsx
@@ -0,0 +1,54 @@
+import React, { useRef, useState, useEffect, ChangeEvent } from "react";
+import { FormValues } from "./AddItemForm";
+
+interface FileInputProps {
+  name: keyof FormValues;
+  value: File | null;
+  onChange: (name: keyof FormValues, value: File | null) => void;
+}
+
+const FileInput: React.FC<FileInputProps> = ({ name, value, onChange }) => {
+  const [preview, setPreview] = useState<string | undefined>();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const nextValue = e.target.files?.[0] || null;
+    onChange(name, nextValue);
+  };
+
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = "";
+    onChange(name, null);
+  };
+
+  useEffect(() => {
+    if (!value) return;
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
+
+    return () => {
+      setPreview(undefined);
+      URL.revokeObjectURL(nextPreview);
+    };
+  }, [value]);
+
+  return (
+    <div className='img-box'>
+      <input type='file' onChange={handleChange} ref={inputRef} />
+
+      {preview && (
+        <div className='img-preview-container'>
+          <img className='img-preview' src={preview} alt='이미지 미리보기' />
+          <button className='img-delete-button' onClick={handleClearClick}>
+            X
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FileInput;
