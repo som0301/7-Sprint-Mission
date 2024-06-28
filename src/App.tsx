5c0332e57d87c12ae348a8c4c4855b57eda358b4
ddol9 <drddol9@gmail.com>
2024-06-28 15:22:34 +0900

    feat: Add App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ItemDetailPage from "./pages/ItemDetailPage/ItemDetailPage.tsx";
+import AddItemPage from "./pages/AddItemPage/AddItemPage";
 import MarketPage from "./pages/MarketPage/MarketPage";
+import NavBar from "./components/NavBar.tsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <MarketPage />
+      <NavBar />
+      <Routes>
+        <Route path='/items' element={<MarketPage />} />
+        <Route path='/items/:productId' element={<ItemDetailPage />} />
+        <Route path='/additem' element={<AddItemPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
