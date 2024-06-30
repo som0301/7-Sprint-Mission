3bf0d9060770d2c256cd6f1d61bc8202b2ef3bae
ddol9 <drddol9@gmail.com>
2024-06-30 15:52:47 +0900

    feat: Add Pagination in Market Page

--- src/pages/MarketPage/components/PrintAllItems.tsx
+++ src/pages/MarketPage/components/PrintAllItems.tsx
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from "react";
 import { getItems } from "../../../api/itemApi";
 import ItemBox, { Item } from "./ItemBox";
 import { Button } from "../../../components/Button/Button";
+import Pagination from "react-js-pagination";
 import "../MarketPage.css";
 
 const PrintAllItems = () => {
   const [items, setItems] = useState<Item[]>([]);
   const [search, setSearch] = useState<string>("");
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemsPerPage: number = 10;
 
   const loadItems = async () => {
-    const items = await getItems({ pageSize: 10, orderBy: "recent" });
+    const items = await getItems({ pageSize: 100, orderBy: "recent" });
     setItems(items.list);
   };
 
@@ -23,18 +26,27 @@ const PrintAllItems = () => {
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
+    setCurrentPage(1);
   };
 
-  const filteredItems = items.filter((item) => 
-    item.name.toLowerCase().includes(search.toLowerCase())
+  const filteredItems = items.filter((item) =>
+    item.name.toLowerCase().includes(search.toLowerCase()),
   );
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredItems.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
 
   return (
     <div className='box-items'>
       <header>
         <span>전체 상품</span>
         <input
-          className="search-input"
+          className='search-input'
           type='text'
           placeholder='검색할 상품을 입력해주세요'
           value={search}
@@ -49,9 +61,19 @@ const PrintAllItems = () => {
       </header>
       <div className='container-items'>
         <div className='all-items'>
-          <ItemBox items={filteredItems} type='all' />
+          <ItemBox items={currentItems} type='all' />
         </div>
       </div>
+
+      <Pagination
+        activePage={currentPage}
+        itemsCountPerPage={itemsPerPage}
+        totalItemsCount={filteredItems.length}
+        pageRangeDisplayed={5}
+        onChange={handlePageChange}
+        itemClass='page-item'
+        linkClass='page-link'
+      />
     </div>
   );
 };
