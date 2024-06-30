1d2e5cf5fcebe25b229986b5d62489d86a547935
ddol9 <drddol9@gmail.com>
2024-06-30 14:16:24 +0900

    feat: 마켓 페이지 검색 기능 추가

--- src/pages/MarketPage/components/PrintAllItems.tsx
+++ src/pages/MarketPage/components/PrintAllItems.tsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { getItems } from "../../../api/itemApi";
-import React from "react";
 import ItemBox, { Item } from "./ItemBox";
 import { Button } from "../../../components/Button/Button";
 import "../MarketPage.css";
 
-const PrintAllItems: React.FC = () => {
+const PrintAllItems = () => {
   const [items, setItems] = useState<Item[]>([]);
+  const [search, setSearch] = useState<string>("");
 
   const loadItems = async () => {
     const items = await getItems({ pageSize: 10, orderBy: "recent" });
@@ -21,10 +21,25 @@ const PrintAllItems: React.FC = () => {
     window.location.href = url;
   };
 
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredItems = items.filter((item) => 
+    item.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <body>
+    <div className='box-items'>
       <header>
         <span>전체 상품</span>
+        <input
+          className="search-input"
+          type='text'
+          placeholder='검색할 상품을 입력해주세요'
+          value={search}
+          onChange={handleSearchChange}
+        />
         <Button
           text='상품 등록하기'
           size='small'
@@ -34,10 +49,10 @@ const PrintAllItems: React.FC = () => {
       </header>
       <div className='container-items'>
         <div className='all-items'>
-          <ItemBox items={items} type='all' />
+          <ItemBox items={filteredItems} type='all' />
         </div>
       </div>
-    </body>
+    </div>
   );
 };
 
