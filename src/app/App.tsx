5de629bd69ebe5f33e5391c4d9e54931dfd78939
reenact11 <jaehyeon11@naver.com>
2024-07-01 15:20:40 +0900

    rename: change jsx to tsx

---
+++ src/app/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GlobalStyle from 'app/styles/GlobalStyle';
+import Header from 'shared/ui/header/Header';
+import AddItem from 'pages/additem/AddItemPage';
+import Items from 'pages/items/ItemsPage';
+import NotFound from 'shared/ui/notfound/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <GlobalStyle />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Items />} />
+        <Route path='/items' element={<Items />} />
+        <Route path='/additem' element={<AddItem />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- src/app/styles/GlobalStyle.tsx
+++ src/app/styles/GlobalStyle.tsx
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
+const GlobalStyle = createGlobalStyle` 
 @font-face {
     font-family: 'Pretendard-Regular';
     src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
