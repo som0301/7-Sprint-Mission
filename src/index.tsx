211a5b0e27ee2770741966eb6ac28c807281e8a9
JiinSeok <seokjiin1073@gmail.com>
2024-07-05 15:55:58 +0900

    ts 변환

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "@/App";
-import "@/styles/global.css"; // index.js에서 global stylesheet을 import하면 전역적으로 스타일이 적용돼요
+import App from "./App";
+import "./styles/global.css"; // index.js에서 global stylesheet을 import하면 전역적으로 스타일이 적용돼요
 import { ThemeProvider } from "styled-components";
-import theme from "@/styles/theme";
-import GlobalStyle from "@/styles/GlobalStyle";
+import theme from "./styles/theme";
+import GlobalStyle from "./styles/GlobalStyle";
 
 const rootElement = document.getElementById("root");
 if (rootElement instanceof HTMLElement) {
