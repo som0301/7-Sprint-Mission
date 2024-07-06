772066596a482d5034594162c80d441820dd0f22
clyde-yoon <a1056719@gmail.com>
2024-07-06 17:21:25 +0900

    기존삭제 후 재시작

--- src/Api.js
+++
@@ -1,9 +0,0 @@
-export async function getData(page, pageSize, order){
-const pageURL = `page=${page}&pageSize=${pageSize}`;
-const query = `${order}`;
-
-const response = await fetch(
-`https://panda-market-api.vercel.app/products?${pageURL}&orderBy=${query}`);
-const body = await response.json()
-return body;
-}
