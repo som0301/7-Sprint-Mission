123c1a73d5314017847851f00440523a142b5ecd
Dangchan Kim <kdc5847@naver.com>
2024-07-07 19:22:09 +0900

    feat: 로그인,회원가입 페이지 유효성 검사 기능 추가

--- src/pages/Authentication/Login.tsx
+++ src/pages/Authentication/Login.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, FocusEvent, useRef, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Authentication.css";
 import logo from "../../assets/logo.svg";
@@ -6,7 +7,141 @@ import visibility_on from "../../assets/btn_visibility_on_24px.svg";
 import google_ic from "../../assets/google-icon.svg";
 import kakao_ic from "../../assets/kakao-icon.svg";
 
+interface InputState {
+  value: string;
+  type?: string;
+  Message: string;
+  isValid: boolean;
+}
+
+function emailCheck(email_address: string) {
+  const email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+
+  if (!email_regex.test(email_address)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
 const Login = () => {
+  const [email, setEmail] = useState<InputState>({
+    value: "",
+    Message: "",
+    isValid: true,
+  });
+  const [password, setPassword] = useState<InputState>({
+    value: "",
+    type: "password",
+    Message: "",
+    isValid: true,
+  });
+  const [disabled, setDisabled] = useState<boolean>(true);
+
+  const eyesImgRef = useRef<HTMLImageElement | null>(null);
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail((prevEmail) => ({
+      ...prevEmail,
+      value: e.target.value,
+    }));
+
+    if (e.target.value.length > 0) {
+      if (email.Message === "이메일을 입력해주세요") {
+        setEmail((prevEmail) => ({
+          ...prevEmail,
+          Message: "",
+        }));
+        e.target.classList.remove("focusout");
+      }
+    }
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword((prevPassword) => ({
+      ...prevPassword,
+      value: e.target.value,
+    }));
+
+    if (e.target.value.length > 0) {
+      if (password.Message === "비밀번호를 입력해주세요") {
+        setPassword((prevPassword) => ({
+          ...prevPassword,
+          Message: "",
+        }));
+        e.target.classList.remove("focusout");
+      }
+    }
+  };
+
+  const handleEmailBlur = (e: FocusEvent<HTMLInputElement>) => {
+    let emailValue = e.target.value;
+
+    if (e.target.value === "") {
+      setEmail((prevEmail) => ({
+        ...prevEmail,
+        Message: "이메일을 입력해주세요",
+        isValid: true,
+      }));
+      e.target.classList.add("focusout");
+    } else if (!emailCheck(emailValue) && e.target.value !== "") {
+      setEmail((prevEmail) => ({
+        ...prevEmail,
+        Message: "잘못된 이메일 형식입니다",
+        isValid: true,
+      }));
+      e.target.classList.add("focusout");
+    } else {
+      setEmail((prevEmail) => ({
+        ...prevEmail,
+        Message: "",
+        isValid: false,
+      }));
+      e.target.classList.remove("focusout");
+    }
+  };
+
+  const handlePasswordBlur = (e: FocusEvent<HTMLInputElement>) => {
+    if (e.target.value === "") {
+      setPassword((prevPassword) => ({
+        ...prevPassword,
+        Message: "비밀번호를 입력해주세요",
+        isValid: true,
+      }));
+      e.target.classList.add("focusout");
+    } else if (e.target.value.length < 8) {
+      setPassword((prevPassword) => ({
+        ...prevPassword,
+        Message: "비밀번호를 8자 이상 입력해주세요",
+        isValid: true,
+      }));
+      e.target.classList.add("focusout");
+    } else {
+      setPassword((prevPassword) => ({
+        ...prevPassword,
+        Message: "",
+        isValid: false,
+      }));
+      e.target.classList.remove("focusout");
+    }
+  };
+
+  const eyesButtonClick = () => {
+    setPassword((prevPassword) => ({
+      ...prevPassword,
+      type: prevPassword.type === "password" ? "text" : "password",
+    }));
+
+    if (eyesImgRef.current) {
+      eyesImgRef.current.src =
+        password.type === "password" ? visibility_off : visibility_on;
+    }
+  };
+
+  useEffect(() => {
+    setDisabled(!(email.isValid === false && password.isValid === false));
+  }, [email.isValid, password.isValid]);
+
   return (
     <div className="body-container">
       <Link to="/" className="auth-logo-link">
@@ -21,26 +156,43 @@ const Login = () => {
             type="email"
             id="email"
             name="email"
+            value={email.value}
             placeholder="이메일을 입력해주세요"
+            onChange={handleEmailChange}
+            onBlur={handleEmailBlur}
           />
+          {email.Message !== "" && (
+            <span className="focusout">{email.Message}</span>
+          )}
           <label htmlFor="password">비밀번호</label>
           <div className="input-container">
             <input
-              type="password"
+              type={password.type}
               id="password"
               name="password"
+              value={password.value}
               placeholder="비밀번호를 입력해주세요"
+              onChange={handlePasswordChange}
+              onBlur={handlePasswordBlur}
             />
-            <button className="btn_visibility" type="button">
+            <button
+              className="btn_visibility"
+              type="button"
+              onClick={eyesButtonClick}
+            >
               <img
                 className="eyes-img"
                 src={visibility_off}
                 alt="비밀번호가리기아이콘"
+                ref={eyesImgRef}
               />
             </button>
           </div>
+          {password.Message !== "" && (
+            <span className="focusout">{password.Message}</span>
+          )}
           <Link to="/items" className="btn_large-link">
-            <button className="btn_large" type="submit" disabled>
+            <button className="btn_large" type="submit" disabled={disabled}>
               로그인
             </button>
           </Link>
